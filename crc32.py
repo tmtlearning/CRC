b38a5264fe9d3086312e63dd82c5fbdad4f2f86b
anonym <mail@example.com>
2012-12-07 22:33:50 +0400

    comments are added

--- crc32.py
+++ crc32.py
@@ -1,10 +1,12 @@
 POLY = 0xEDB88320
 
 def crc32(data):
-	'''crc32 calculating function'''
+	'''crc32 calculating function
+	   see README for more information about algorithm realization
+	'''
 	crc_table = [0]*256
 
-	for i in range(256):	#l
+	for i in range(256):	#crc32 initialization table calculation cycle
 		cell = i
 		for j in range(8):
 			if cell & 1:
@@ -14,7 +16,7 @@ def crc32(data):
 		crc_table[i] = cell
 
 	crc = 0xffffffff
-	for c in data:
+	for c in data:		#crc32 calculation cycle
 		crc = (crc >> 8) ^ crc_table[(crc ^ ord(c)) & 0xff]
 	return hex(crc ^ 0xfffffffF)
 
